74f404ee4d98e72daf0b3ee853214cc32b9673e2
Warren Wiser <wswiser101@utexas.edu>
2022-10-06 03:39:06 +0200

    started basic setup, adding a tab vc to control the pages we want (the number/content of these tabs will be perfected later)

---
+++ Group23/pages/MergeVC.swift
@@ -0,0 +1,29 @@
+//
+//  MergeVC.swift
+//  Group23
+//
+//  Created by m1 on 06/10/2022.
+//
+
+import UIKit
+
+class MergeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
