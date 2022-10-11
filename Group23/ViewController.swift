7d920ad406762a969bb8fc2d774224ba4f876a07
Warren Wiser <wswiser101@utexas.edu>
2022-10-11 04:10:03 +0200

    Added dark/light mode pallets; basic setup with Tab bar controller after meeting feedback

--- Group23/ViewController.swift
+++ Group23/ViewController.swift
@@ -2,18 +2,17 @@
 //  ViewController.swift
 //  Group23
 //
-//  Created by m1 on 06/10/2022.
+//  Created by m1 on 11/10/2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITabBarController {
+    
+    @IBOutlet weak var tabBarElement: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
