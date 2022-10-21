7f4425c4e54ab2622b174ac77d5e61375801bcf1
Warren Wiser <wswiser101@utexas.edu>
2022-10-21 19:40:02 +0200

    fixed second tap of profile icon always presenting the login screen

--- Group23/ViewController.swift
+++ Group23/ViewController.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class ViewController: UITabBarController, UITabBarControllerDelegate {
     
     @IBOutlet weak var tabBarElement: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // allow disabling diboule tab of profile icon
+        delegate = self
+    }
+
+    // disables login screen presentation on second tap of profile tab element
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let tabIdx = tabBarController.viewControllers?.firstIndex(of: viewController)
+        return tabIdx != 2 || tabIdx != tabBarController.selectedIndex
     }
 
 }
--- Group23/pages/HomeVC.swift
+++ Group23/pages/HomeVC.swift
@@ -7,17 +7,11 @@
 
 import UIKit
 
-class HomeVC: UIViewController, UITabBarControllerDelegate {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        // todo this should prevent the profile page from popping to the login page on second click
-        let tabIdx = tabBarController.viewControllers?.firstIndex(of: viewController)
-        return tabIdx != 2 || tabIdx != tabBarController.selectedIndex
     }
 }
