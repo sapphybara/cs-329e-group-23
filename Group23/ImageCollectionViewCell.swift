3a447ac3f7989d53407017d14d423703cdab1aa6
Maxwell <maxwellkret@gmail.com>
2022-10-24 17:19:22 -0500

    ScanVC init - max

---
+++ Group23/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  Group23
+//
+//  Created by Maxwell Kretschmer on 10/21/22.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+	@IBOutlet weak var cellImageView: UIImageView! // must be connected to UIImageView
+	
+}
--- Group23/pages/ScanVC.swift
+++ Group23/pages/ScanVC.swift
@@ -6,24 +6,60 @@
 //
 
 import UIKit
+import VisionKit
 
 class ScanVC: UIViewController {
+	
+	var imageArray: [UIImage] = []
+	
+	@IBOutlet weak var initialTextLabel1: UILabel!
+	@IBOutlet weak var initialTextLabel2: UILabel!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		// configureDocumentView()
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	@IBAction func cameraButtonPressed(_ sender: UIButton) {
+		configureDocumentScanView()
+	}
+	
+	@IBAction func libraryButtonPressed(_ sender: UIButton) {
+//		TODO: Upload Files
+	}
+	
+	
+	private func configureDocumentScanView() {
+		let documentViewVC = VNDocumentCameraViewController()
+		documentViewVC.delegate = self
+		self.present(documentViewVC, animated: true)
+	}
+	
 }
+
+extension ScanVC:VNDocumentCameraViewControllerDelegate {
+	func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+		for pageNum in 0..<scan.pageCount {
+			let image = scan.imageOfPage(at: pageNum)
+			print("image: \(image)") // for debug only
+			self.imageArray.append(image)
+		}
+		self.dismiss(animated: true)
+		print("imageArray: \(imageArray)")
+
+//		Add this to self.dismiss() reload table/collection data after adding to imageArray
+//		{
+//			DispatchQueue.main.async {
+//				self.myCollectionOrTableView.reloadData()
+//			}
+//		}
+		
+	}
+}
+
+
