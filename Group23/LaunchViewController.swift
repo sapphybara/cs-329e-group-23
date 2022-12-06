1e2601bde64943c6f06f379d7e6d7818bfe6ff50
Maxwell <maxwellkret@gmail.com>
2022-12-05 22:37:28 -0600

    finished LaunchViewController.swift

--- Group23/LaunchViewController.swift
+++ Group23/LaunchViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class LaunchViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var progressView: UIProgressView!
+	
+	let segueID = "launchDoneSegue"
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+		// Do any additional setup after loading the view.
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		CATransaction.setCompletionBlock({ 
+			print("Done!")
+			self.performSegue(withIdentifier: self.segueID, sender: nil)
+		})
+		
+		UIView.animate(withDuration: 4.0) {
+			self.progressView.setProgress(1.0, animated: true)
+		}
+	}
+	
 }
